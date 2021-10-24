f3ab88c08b292535ff95f09027362a6173f9ad98
8astien <bastien.cauquil@pm.me>
2021-10-24 12:36:09 +0200

    UI update and functionnalities added

--- interface.py
+++ interface.py
@@ -1,42 +1,78 @@
+from os import wait
 import tkinter
 from tkinter import *
 from tkinter import Text,ttk
-from PIL import Image, ImageFont, ImageDraw, ImageOps
+from PIL import Image, ImageFont, ImageDraw, ImageOps,ImageTk
 import textwrap
 
 window = Tk() #create a window
 
 # add widgets here
 
-
 #================================== UI Elements ===========================================================
 
 inputSpell = Entry(window, text="", bd=5)
-inputSpell.place(x=600, y=50)
+inputSpell.place(x=1150, y=50)
 
 inputDesc = Text(window , width=40 , height=10)
-inputDesc.place(x=600, y=150 )
+inputDesc.place(x=1150, y=150 )
 
-
-dropType = ttk.Combobox(window, 
+dropDamage = ttk.Combobox(window,
                             values=[
-                                    "Force", 
+                                    "1",
+                                    "2",
+                                    "3",
+                                    "4",
+                                    "5",
+                                    "6"])
+print(dict(dropDamage))
+dropDamage.grid(column=0, row=1)
+dropDamage.place(x=1150 ,y=350)
+dropDamage.current(0)
+
+dropDice = ttk.Combobox(window,
+                            values=[
+                                    "d4",
+                                    "d6",
+                                    "d8",
+                                    "d10",
+                                    "d12"])
+print(dict(dropDice))
+dropDice.grid(column=0, row=1)
+dropDice.place(x=1150 ,y=400)
+dropDice.current(0)
+
+inputBonusDamage = Entry(window, text="", bd=5)
+inputBonusDamage.place(x=1150, y=450)
+
+inputMana = Entry(window, text="", bd=5)
+inputMana.place(x=1150, y=500)
+
+dropType = ttk.Combobox(window,
+                            values=[
+                                    "Force",
                                     "Agilité",
                                     "Intelligence",
                                     "Passif"])
-print(dict(dropType)) 
+print(dict(dropType))
 dropType.grid(column=0, row=1)
-dropType.place(x=600 ,y=100)
+dropType.place(x=1150 ,y=100)
 dropType.current(0)
+
+canvas = Canvas(window , width=750 , height=1050)
+canvas.place(x=100 , y= 0)
+#img = PhotoImage(file="./ressources/GeneralTemplate.png")
+#canvas.create_image(20,20, anchor=NW, image=img)
 #================================== UI Elements ===========================================================
 
 #================================================================================================================
-
 
 cardTemplate = Image.open("ressources/GeneralTemplate.png").convert("RGBA") # This is the general PNG template used for cards
 titleFont = ImageFont.truetype('ressources/enchantedLand.otf', 60)
 typeFont = ImageFont.truetype('ressources/Roboto-Regular.ttf', 30)
 descFont = ImageFont.truetype('ressources/Roboto-Regular.ttf', 26)
+statFont = ImageFont.truetype('ressources/enchantedLand.otf', 80)
+
 # fonts color variables
 forceColor = ('#EA2727')
 agilityColor = ('#E7E743')
@@ -52,7 +88,6 @@ wPercent = (maxWidth/float(bckgImage.size[0])) # calculate ratio width between t
 newSize = int((float(bckgImage.size[1])*float(wPercent))) #Store the calculated height ratio
 bckgImage = bckgImage.resize((maxWidth,newSize), Image.ANTIALIAS) #apply new ratio to var
 
-
 def add_margin(bckgImage, top, right, bottom, left, color): # creating add_margin function for later use
     width, height = bckgImage.size
     new_width = width + right + left
@@ -66,12 +101,10 @@ bckgImage = add_margin(bckgImage, 0, 0, (1050-newSize), 0, (120, 120, 120)) # ad
 # Using a wrapper for desc as it is usually long texts.
 
 def getSpellName():
-    spellName = inputSpell.get()
-    return spellName
+    return inputSpell.get()
 
 def getSpellType():
-    spellType = dropType.get()
-    return spellType
+    return dropType.get()
 
 def getDesc(editCard):
     spellDesc = inputDesc.get("1.0","end-1c")
@@ -83,6 +116,16 @@ def getDesc(editCard):
     spellDescWrapped += wordList[-1]
     editCard.text((40,750), spellDescWrapped, '#333232', font = descFont, anchor = 'ls')
     return spellDesc
+
+def getDamage(editCard):
+    print("Test bonus : " + inputBonusDamage.get())
+    if(inputBonusDamage.get() != ""):
+        editCard.text((100, 1000), dropDamage.get()+dropDice.get()+"+"+inputBonusDamage.get(), forceColor, font = statFont, anchor = 'ms', stroke_width = 3, stroke_fill = outline) # Title of the card, big text on top
+    else:
+       editCard.text((100, 1000), dropDamage.get()+dropDice.get(), forceColor, font = statFont, anchor = 'ms', stroke_width = 3, stroke_fill = outline) # Title of the card, big text on top
+
+def getMana(editCard):
+    editCard.text((685, 1000), inputMana.get(), intelligenceColor, font = statFont, anchor = 'ms', stroke_width = 3, stroke_fill = outline)
 
 def editCard(nameSpell, typeSpell , editCard):
     editCard = ImageDraw.Draw(cardTemplate)
@@ -105,28 +148,42 @@ def editCard(nameSpell, typeSpell , editCard):
 
 #=================================================================================================================
 
-def save(): 
+def save():
+
     spellName = getSpellName()
     spellType = getSpellType()
     spellDesc = getDesc(editableCard)
+    getDamage(editableCard)
+    getMana(editableCard)
     editCard(spellName , spellType , editableCard)
+    img = ImageTk.PhotoImage(Image.open("./ressources/GeneralTemplate.png"))
+    canvas.config(img)
     bckgImage.paste(cardTemplate, (0, 0), cardTemplate)
     bckgImage.save('Cards/'+spellType+'_'+spellName+'.png')
+    print('Test finish')
+
     print('Created card succesfully !')
 
-
 btn = Button(window,text="Generer" ,command=save)
-btn.place(x=900, y=50)
+btn.place(x=1200, y=550)
 labelName = Label(window, text = "Card Name")
-labelName.place(x=450, y=50)
+labelName.place(x=950, y=50)
 labelType = Label(window, text = "Card Type")
-labelType.place(x=450, y=100)
+labelType.place(x=950, y=100)
 labelDesc = Label(window, text = "Card Description")
-labelDesc.place(x=450, y=150)
+labelDesc.place(x=950, y=150)
+labelDamage = Label(window, text = "Damage")
+labelDamage.place(x=950, y=350)
+labelDice = Label(window, text = "Dice")
+labelDice.place(x=950, y=400)
+labelMana = Label(window, text = "Mana")
+labelMana.place(x=950, y=500)
+labelBonusD = Label(window, text = "Bonus Damage")
+labelBonusD.place(x=950, y=450)
 #================================== UI Window ===========================================================
 
 window.title('CardGen pre-release version 0.000000000432')
-window.geometry("1024x720+10+20")
+window.geometry("1500x1050+10+20")
 window.mainloop() #mainloop displays the window
 
 #================================== UI Window ===========================================================
